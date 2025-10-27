140ec5c7a1e18459b4c689ba2c19432bcbded225
Anmol Rajbhandari <anmolrajbhandari12@gmail.com>
2025-10-27 23:20:22 +0545

    Connected JavaScript to clock, added functional hand rotations and modification

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('Analog clock script connected');
+const hourhand = document.getElementById("hourhand");
+const minuteshand = document.getElementById("minuteshand");
+const secondshand = document.getElementById("secondshand");
+
+function updateClock(){
+    const time = new Date();
+    const gethour = time.getHours();
+    const getminute = time.getMinutes();
+    const getseconds = time.getSeconds();
+    const hoursdeg = gethour*30 +getminute/2; 
+    const minutesDeg = getminute*6; 
+    
+    const secondsDeg = getseconds*6;                       
+    hourhand.style.transform = `rotate(${hoursdeg + 180}deg )`;
+    minuteshand.style.transform = `rotate(${minutesDeg + 180}deg)`;
+    secondshand.style.transform = `rotate(${secondsDeg}deg)`;
+}
+
+updateClock();
+setInterval(updateClock,1000)
